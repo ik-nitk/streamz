1641607f283bb8799fb5b1fb87f6840ae327b0a0
shambhavik142 <32361260+shambhavik142@users.noreply.github.com>
2019-03-13 23:08:36 +0530

    Update model.py

--- search_module/backend_server/model.py
+++ search_module/backend_server/model.py
@@ -1,21 +1,27 @@
+#import libraries
 import web
 import json
 import requests
-# Import Elasticsearch package 
 
+# Import Elasticsearch package 
 from elasticsearch import Elasticsearch 
+
+
 # function to upload details to server
+#details consists of id,channel,uploader,category,title,description and tags
 def send_details(details):
   ids=json.loads(details)['id']
   # Connect to the elastic cluster
   es=Elasticsearch([{'host':'localhost','port':9200}])
-  res = es.index(index='streams1',doc_type='video',body=details,id=ids)
+  res = es.index(index='streams1',doc_type='video',body=details,id=ids)  #index_name=streams1, doc_type=videos
   return res
 
+
 # function to search by keywords
+#search based on category,channel,uploader,title,tags
 def send_search(txt):
   txt=str(txt)
-  print(txt)
+  #print(txt)
   videos=[]
   es=Elasticsearch([{'host':'localhost','port':9200}])
   res= es.search(index='streams1',body={"query" : {
@@ -35,10 +41,11 @@ def send_search(txt):
     videos.append(hit['_source']['id'])
   ids = {'id': videos}
   return ids
+
   
  # function to delete from server 
 def delete(index):
-  print index
+  #print index
   es=Elasticsearch([{'host':'localhost','port':9200}])
   res = es.delete(index='streams1', doc_type='video', id=index)
   return res	
