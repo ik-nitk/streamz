37fa29f4f2b8af0bf307e55c7336cfe2fd9c378e
shambhavik142 <32361260+shambhavik142@users.noreply.github.com>
2019-03-13 22:39:51 +0530

    Delete model.py

--- streams_search/streams_search/hosting_server/model.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-url = 'http://0.0.0.0:3000/'
-
-def send_search(search_text):
-    keywords = {'keyword': search_text} 
-    requests.post(url, data=json.dumps(keywords)) 
